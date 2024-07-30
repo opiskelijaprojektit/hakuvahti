86e1228fa21cdff6afdc4f467a7b39a6dffe6508
Henrik Blom <p44261@edu.sasky.fi>
2024-07-30 16:59:08 +0300

    tyylien ja asettelun muokkausta

--- src/view/uusihaku.php
+++ src/view/uusihaku.php
@@ -1,23 +1,18 @@
 <?php $this->layout('template', ['title' => 'Haku']) ?>
 
-<h1>Lisää haku</h1>
-
-<form action="" method="POST">
-<table>
-  <tr>
-    <th></th>
-    <th></th>
-  </tr>
-  <tr>
-    <td><label for="hakusana">Haettu sana:</label></td>
-    <td><input id="hakusana" type="text" name="hakusana" class="hakukentta"></td>
-  </tr>
-  <tr>
-    <td><label for="email">Sähköposti:</label></td>
-    <td><input id="email" type="text" name="email" class="spostikentta"></td>
-  </tr>
-</table>
+<div class="content_box">
+  <h2>Lisää haku</h2>
   <div>
-    <input type="submit" name="laheta" value="Lisää haku" class="button">
+    <form action="" method="POST" class="input_form">
+      <table>
+        <h3><label for="hakusana">Haettu sana:</label></h3>
+        <input id="hakusana" type="text" name="hakusana" class="input_field">
+        <h3><label for="email">Sähköposti:</label></h3>
+        <input id="email" type="text" name="email" class="input_field">
+      </table>
+      <div>
+        <input type="submit" name="laheta" value="Lisää haku" class="button">
+      </div>
+    </form>
   </div>
-</form>
+</div>
