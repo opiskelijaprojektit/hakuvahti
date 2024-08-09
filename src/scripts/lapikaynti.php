55e6f6b00eb452459d61cf3fdf185e2e96104842
ansu <annastiina.koivu@gmail.com>
2024-08-09 20:04:34 +0300

    lisää toiminnallisuuden, joka hakee kurssidatasta käyttäjän hakusanan sisältävät kurssit

--- src/scripts/lapikaynti.php
+++ src/scripts/lapikaynti.php
@@ -31,65 +31,57 @@ $kayttajat = hae_kayttajat();
 foreach ($kayttajat as $kayttaja => $tiedot) {
     $idkayttaja = $tiedot['idkayttaja'];
     $email = $tiedot['email'];
-    echo "Käyttäjä:" . $email . "\n";
-    echo "----------" . "\n";
+    echo "Käyttäjä: " . $email . "\n";
+    echo "--------------------------------------" . "\n";
 
     // Haetaan kyseisen käyttäjän kaikki hakusanat
     $kayttaja_hakusanat = hae_kayttaja_hakusanat($idkayttaja);
 
     // Käydään hakusanat yksitellen läpi
     foreach ($kayttaja_hakusanat as $key => $value) {
-        echo "Hakusana:" . $value['hakusana'] . "\n";
+        $hakusana = $value['hakusana'];
+        echo "Hakusana: " . $hakusana . "\n";
+        echo "++++++++" . "\n";
+        echo "\n";
+
+        // Käydään läpi haettu kurssidata,
+        // ja etsitään sieltä hakusanaa.
+
+        // Käydään läpi koulu kerrallaan
+        foreach($kurssit_haun_mukaan as $schooldata) {
+            // Käydään koulun kurssit yksitellen läpi
+            foreach ($schooldata["Courses"] as $coursedata) {
+                // Katsotaan sisältääkö kurssi dataa
+                if(isset($coursedata["text"])) {
+                    $teksti = $coursedata["text"];
+                    $hash = $coursedata["hash"];
+                    
+                    // Etsitään hakusanaa kurssidatasta
+                    if(stripos($teksti, $hakusana) !== false) { 
+                        // Hakusana löytyi, tulostetaan komentoriville testikäytössä.
+                        echo "Koulutus: " . $coursedata['Koulutus'] . "\n";
+                        echo "Oppilaitos: " . $coursedata['Oppilaitos'] . "\n";
+                        echo "-------\n";
+                
+                        // //hakusana löytyi, lähetetään sähköpostia tilaajalle
+                        // $html_pohja = file_get_contents('sahkoposti_pohja.html'); //Luodaan HTML-pohja
+                        // $subject = "Hakusanasi löytyi koulutuksista";
+                        
+                        // $message = str_replace('[HAKUSANA]', $hakusana, $html_pohja); //Korvaa [HAKUSANA]-kohdan HTML-pohjassa
+                
+                        // $headers = "MIME-Version: 1.0" . "\r\n";
+                        // $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+                        // $headers .= "From: neutroni.hayo.fi";
+                        
+                
+                        // mail($email, $subject, $message, $headers);
+
+                    }
+                }
+            }
+        }
     }
-    echo "----------" . "\n";
 }
-
-
-// $tulos = lapikaynti_sql_lause(); // Suritetaan kysely
-// 
-
-// foreach($tulos as $rivi) {
-//   $email = $rivi['email'];
-//   $hakusana = $rivi['hakusana'];
-  
-//   $hakusana_loytyy = false; // alustaa muuttujan false:ksi oletuksena
-
-//   // Vertaillaan kurssien nimiä tietokannan hakusanoihin
-//   foreach($kurssit_haun_mukaan as $schooldata) { 
-//     foreach ($schooldata["Courses"] as $coursedata) {
-//       if(isset($coursedata["text"])) {
-//         $teksti = $coursedata["text"];
-//         $hash = $coursedata["hash"];
-      
-//         if(stripos($teksti, $hakusana) !== false) { 
-//           //hakusana läytyi, tulostetaan komentoriville testikäytössä.
-//           echo "Käyttäjä: $email\n";
-//           echo "Hakusana: $hakusana löytyi koulutuksesta. \n";
-//           echo "-------\n";
-
-//           //hakusana löytyi, lähetetään sähköpostia tilaajalle
-//           $html_pohja = file_get_contents('sahkoposti_pohja.html'); //Luodaan HTML-pohja
-//           $subject = "Hakusanasi löytyi koulutuksista";
-          
-//           $message = str_replace('[HAKUSANA]', $hakusana, $html_pohja); //Korvaa [HAKUSANA]-kohdan HTML-pohjassa
-
-//           $headers = "MIME-Version: 1.0" . "\r\n";
-//           $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-//           $headers .= "From: neutroni.hayo.fi";
-          
-
-//           mail($email, $subject, $message, $headers);
-//           $hakusana_loytyy = true;
-//           break;
-//         }
-//       }
-//     }
-//     if($hakusana_loytyy) {
-//       break; // ei tarvitse jatkaa
-//     }
-//   }
-// }
-
 
 
 ?>
